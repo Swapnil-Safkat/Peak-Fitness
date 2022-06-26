06f05614ff5d8570b732f7a4499d13a42f52384c
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-06-26 18:10:14 +0600

    navbar done

---
+++ app.js
@@ -0,0 +1,23 @@
+//get all the sections
+const sections = [
+  document.getElementById("section-bmi"),
+  document.getElementById("section-bmr"),
+  document.getElementById("section-ideal-weight"),
+  document.getElementById("section-calorie")
+];
+
+//manage view
+const viewManager = (view) => {
+  sections.map(sec => {
+    sec.classList.replace("block", "hidden");
+  })
+  let index = 0;
+  for (let v of view) {
+    if (v) {
+      sections[index].classList.replace("hidden", "block");
+      break;
+    }
+    index++;
+  };
+};
+viewManager([true, false, false, false])
